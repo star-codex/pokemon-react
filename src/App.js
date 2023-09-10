801adae52e6db968aa4749aed9c6e43f7572c1b5
virgo <aerynn@null.net>
2023-09-11 10:23:28 +1200

    installed axiom, modified package.json for SSL workaround, added get request in app.js for API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import PokemonList from './pokemonList';
+import axios from 'axios';
 
 function App() {
 	const [pokemon, setPokemon] = useState(['bulbasaur', 'charmander']);
+
+	axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => {
+		setPokemon(res.data.results.map((p) => p.name));
+	});
 
 	return <PokemonList pokemon={pokemon} />;
 }
