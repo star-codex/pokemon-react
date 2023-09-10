a2c4884f8fa3b2d65e0bbcae91c8c5a6b7b8d0b7
virgo <aerynn@null.net>
2023-09-11 10:43:09 +1200

    current page variables added, get request inside useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './pokemonList';
 import axios from 'axios';
 
 function App() {
-	const [pokemon, setPokemon] = useState(['bulbasaur', 'charmander']);
+	const [pokemon, setPokemon] = useState([]);
+	const [currentPageUrl, setCurrentPageUrl] = useState(
+		'https://pokeapi.co/api/v2/pokemon'
+	);
 
-	axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => {
-		setPokemon(res.data.results.map((p) => p.name));
-	});
+	// every time currentPageUrl changes, rerun the code inside of it; if it doesn't, do not rerender it.
+	useEffect(() => {
+		axios.get(currentPageUrl).then((res) => {
+			setPokemon(res.data.results.map((p) => p.name));
+		});
+	}, [currentPageUrl]);
 
 	return <PokemonList pokemon={pokemon} />;
 }
