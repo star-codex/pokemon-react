b9aed1f25fdc1253597f54bfacd08a1ef4f15a60
virgo <aerynn@null.net>
2023-09-11 10:49:26 +1200

    next and previous states implemented

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ function App() {
 	const [currentPageUrl, setCurrentPageUrl] = useState(
 		'https://pokeapi.co/api/v2/pokemon'
 	);
+	const [nextPageUrl, setNextPageUrl] = useState();
+	const [prevPageUrl, setPrevPageUrl] = useState();
 
 	// every time currentPageUrl changes, rerun the code inside of it; if it doesn't, do not rerender it.
 	useEffect(() => {
 		axios.get(currentPageUrl).then((res) => {
+			setNextPageUrl(res.data.next);
+			setPrevPageUrl(res.data.previous);
 			setPokemon(res.data.results.map((p) => p.name));
 		});
 	}, [currentPageUrl]);
